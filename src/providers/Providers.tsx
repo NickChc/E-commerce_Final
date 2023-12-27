b29f42ea8d2b36b59329df3db45402e89ebefce4
NickChc <nika.chichua26@gmail.com>
2023-12-27 15:49:02 +0400

    feat: providers updated

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -2,12 +2,15 @@ import { PropsWithChildren } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalProvider } from "@src/providers/GlobalProvider";
 import { ThemeProvider } from "@src/providers/ThemeProvider";
+import { LocaleProvider } from "@src/providers/LocaleProvider";
 
 export function Providers({ children }: PropsWithChildren) {
   return (
     <BrowserRouter>
       <GlobalProvider>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <LocaleProvider>{children}</LocaleProvider>
+        </ThemeProvider>
       </GlobalProvider>
     </BrowserRouter>
   );
