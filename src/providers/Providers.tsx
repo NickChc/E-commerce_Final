ecc042dbef1fbcdcd95aaab329aba1b3c1e0c669
NickChc <nika.chichua26@gmail.com>
2023-12-27 15:13:50 +0400

    feat: providers added

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,9 +1,14 @@
 import { PropsWithChildren } from "react";
 import { BrowserRouter } from "react-router-dom";
-
+import { GlobalProvider } from "@src/providers/GlobalProvider";
+import { ThemeProvider } from "@src/providers/ThemeProvider";
 
 export function Providers({ children }: PropsWithChildren) {
-  return <BrowserRouter>
-  {children}
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <GlobalProvider>
+        <ThemeProvider>{children}</ThemeProvider>
+      </GlobalProvider>
+    </BrowserRouter>
+  );
 }
