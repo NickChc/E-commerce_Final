b66ebe893f5a4132555eab8e1975cedf5cb95575
NickChc <nika.chichua26@gmail.com>
2023-12-28 21:47:05 +0400

    style: button hover bg

--- src/components/Button/Button.ts
+++ src/components/Button/Button.ts
@@ -1,20 +1,23 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import tw from "twin.macro";
 
-
 interface SButtonProps {
-    variation?: string;
+  variation?: string;
 }
 
 export const SButton = styled.button<SButtonProps>`
-  ${tw`p-3 px-6 m-1 rounded-xl border-solid border cursor-pointer flex items-center justify-center gap-[18px] `}
+  ${tw`p-3 px-6 m-1 rounded-xl font-semibold border-solid border cursor-pointer flex items-center justify-center gap-[18px] `}
   border-color: #1F51FF;
   background-color: ${(props) => props.variation === "active" && "#1F51FF"};
   color: ${(props) => props.variation === "active" && "#ffffff"};
+  transition: all 0.3s;
+
+  :hover {
+    background-color: #add8e6;
+  }
 
   div {
     ${tw`text-2xl`}
     color: ${(props) => (props.variation === "active" ? "#ffffff" : "#1F51FF")};
   }
 `;
-
